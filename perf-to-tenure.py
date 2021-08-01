c819b0ab2b2b37823206c187814988cae0add076
Sean Carey <sean@densone.com>
2021-07-31 20:44:13 -0400

    Create perf-to-tenure.py
    first commit

---
+++ perf-to-tenure.py
@@ -0,0 +1,31 @@
+import requests
+import json
+
+v_data = []
+
+def get_data(cursor='none'):
+    if (cursor != "none"): 
+        uri =  "https://api.helium.io/v1/validators?cursor="+cursor
+    else: 
+        uri =  "https://api.helium.io/v1/validators"
+    
+    r = requests.get(uri)
+    data = json.loads(r.text)
+    for d in data['data']:
+        v_data.append(d)
+    if ("cursor" in data):
+        get_data(data['cursor'])
+ten = 0
+pen = 0
+get_data()
+for v in v_data: 
+    if (v['stake_status'] == "staked"):
+        for p in v['penalties']:
+            if p['type'] == 'dkg':
+                pen = pen+1
+            if p['type'] == 'performance':
+                pen = pen+p['amount']
+            if p['type'] == 'tenure':
+                ten = ten+0.5
+
+print(pen/ten)
